fa950aad9ffe9f3a5d35181dac30ed7d55291a06
MaximilianoAlonso <Elgrungemaxi1986@gmail.com>
2023-02-15 09:18:41 +0100

    terminado

---
+++ src/controllers/mainController.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+const controller = {
+	index: (req, res) => {
+		// Do the magic
+		
+		const inSale = products.filter(product => product.category === "in-sale")
+		const visited = products.filter(product => product.category === "visited")
+
+		return res.render("index",{
+			toThousand,
+			inSale,
+			visited,
+		})
+	},
+	search: (req, res) => {
+		const {keywords} = req.query;
+		const searchProduct = products.filter(product =>
+			product.name.toLowerCase().includes(keywords.toLowerCase()) || product.description.toLowerCase().includes(keywords.toLowerCase()));
+			return res.render("results",{
+				
+				keywords,
+				searchProduct,
+				toThousand
+
+			})
+		
+	},
+};
+
+module.exports = controller;
